b3dae79fda052058dd63ea680665e864bbcf1203
Gelo La Madrid <hafallamadrid@gmail.com>
2023-12-05 08:37:30 +0800

    Modified Home screen

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -18,6 +18,9 @@ const loadFonts = async () => {
 };
 
 const HomePage = ({ navigation }) => {
+  const goToHome = () => {
+    navigation.navigate("Home");
+  };
   const goToProfile = () => {
     navigation.navigate("Profile");
   };
@@ -42,82 +45,66 @@ const HomePage = ({ navigation }) => {
               />
               <View style={styles.userInfo}>
                 <View>
-                  <Text style={styles.greetingText}>Good Day, Jane!</Text>
-                  <TouchableOpacity onPress={goToProfile}>
-                    <Text style={styles.profileButton}>My Profile</Text>
-                  </TouchableOpacity>
+                  <Text style={styles.greetingText}>Good Day!</Text>
                 </View>
-                <Image
-                  source={require("../assets/user.png")}
-                  style={styles.avatar}
-                />
               </View>
             </View>
           </ImageBackground>
         </View>
 
-        {/* Section for Daily Skin Log */}
-        <View style={styles.dailySkinLogSection}>
-          <ImageBackground
-            source={require("../assets/skin-log.png")}
-            style={styles.skinLogBG}
-          >
-            <View style={styles.ellipseShape} />
-            <View style={styles.dailySkinLogContent}>
-              <View style={styles.dailySkinLogTitle}>
-                <Image
-                  source={require("../assets/icons/date-icon.png")}
-                  style={styles.icons}
-                />
-                <Text style={styles.logTitleText}>Daily Skin Log</Text>
-              </View>
-              <Text style={styles.questionText}>
-                Tell us how is your skin feeling today?
-              </Text>
-              <View style={styles.emoticonButtons}>
-                {/* Add emoticon buttons here */}
-                <TouchableOpacity style={styles.moodButton}>
-                  <Icon name="mood" size={24} color="#7D70BA" />
-                  <Text>Amazing</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.moodButton}>
-                  <Icon name="mood" size={24} color="#7D70BA" />
-                  <Text>Amazing</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.moodButton}>
-                  <Icon name="mood" size={24} color="#7D70BA" />
-                  <Text>Amazing</Text>
-                </TouchableOpacity>
-
-                {/* Add more buttons as needed */}
-              </View>
-            </View>
-          </ImageBackground>
-        </View>
-
-        {/* Section for Skin Health */}
-        <View style={styles.skinHealthSection}>
-          <Text style={styles.skinHealthText}>Skin Health</Text>
-          {/* Skin Analysis Box */}
-          {/* Include visual representation, progress bar, score, time, and analyze button */}
-          <TouchableOpacity style={styles.analyzeButton} onPress={goToCamera}>
-            <Icon name="camera-alt" size={24} color="#7D70BA" />
-            <Text>Analyze Skin</Text>
+        <View style={styles.skinProblemSection}>
+          <TouchableOpacity style={styles.skinProblemButtons}>
+            <Image
+              source={require("../assets/Blackheads-button.png")}
+              style={styles.skinButton}
+            />
+            <Text style={styles.recommenderText}>Lorem Ipsum </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.skinProblemButtons}>
+            <Image
+              source={require("../assets/DarkSpots-button.png")}
+              style={styles.skinButton}
+            />
+            <Text style={styles.recommenderText}>Lorem Ipsum </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.skinProblemButtons}>
+            <Image
+              source={require("../assets/Nodules-button.png")}
+              style={styles.skinButton}
+            />
+            <Text style={styles.recommenderText}>Lorem Ipsum </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.skinProblemButtons}>
+            <Image
+              source={require("../assets/Papules-button.png")}
+              style={styles.skinButton}
+            />
+            <Text style={styles.recommenderText}>Lorem Ipsum </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.skinProblemButtons}>
+            <Image
+              source={require("../assets/Pustules-button.png")}
+              style={styles.skinButton}
+            />
+            <Text style={styles.recommenderText}>Lorem Ipsum </Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.skinProblemButtons}>
+            <Image
+              source={require("../assets/Whitehead-button.png")}
+              style={styles.skinButton}
+            />
+            <Text style={styles.recommenderText}>Lorem Ipsum </Text>
           </TouchableOpacity>
         </View>
+
+        <View style={styles.Gap}></View>
       </ScrollView>
 
       {/* Bottom Toolbar */}
       <View style={styles.bottomToolbar}>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={goToHome}>
           <Image
             source={require("../assets/icons/home-icon.png")}
-            style={styles.toolbarIcons}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image
-            source={require("../assets/icons/heart-icon.png")}
             style={styles.toolbarIcons}
           />
         </TouchableOpacity>
@@ -134,12 +121,6 @@ const HomePage = ({ navigation }) => {
             />
           </TouchableOpacity>
         </View>
-        <TouchableOpacity>
-          <Image
-            source={require("../assets/icons/notifs-icon.png")}
-            style={styles.toolbarIcons}
-          />
-        </TouchableOpacity>
         <TouchableOpacity onPress={goToProfile}>
           <Image
             source={require("../assets/icons/user-icon.png")}
@@ -195,123 +176,23 @@ const styles = StyleSheet.create({
     textAlign: "center",
     paddingVertical: 10,
   },
-  profileButton: {
-    color: "#7D70BA",
-    backgroundColor: "rgba(128,128,128, 0.3)",
-    textAlign: "center",
-    fontSize: 14,
-    fontWeight: "bold",
-    marginTop: 4,
-    padding: 10,
-    borderRadius: 15,
-    zIndex: 1,
-    width: 150,
-  },
-  avatar: {
-    width: 160,
-    height: 160,
-    borderRadius: 20,
-    marginLeft: 0,
-    right: -50,
-    bottom: -5,
-  },
-  dailySkinLogSection: {
-    overflow: "hidden",
-    height: 283,
-    marginVertical: 20,
-    marginHorizontal: 25,
-    borderRadius: 15,
-    elevation: 4,
-  },
-  skinLogBG: {
-    flex: 1,
-    resizeMode: "contain",
+  skinProblemSection: {
     alignItems: "center",
-    justifyContent: "center",
-  },
-  ellipseShape: {
-    paddingHorizontal: 0,
-    backgroundColor: "white",
-    width: 250,
-    height: 250,
-    borderRadius: 125,
-    transform: [{ scaleX: 2 }],
-    zIndex: 0,
-    top: 100,
-  },
-  dailySkinLogContent: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    zIndex: 1,
-    bottom: 80,
-  },
-  dailySkinLogTitle: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 10,
-    marginBottom: 12,
-  },
-  logTitleText: {
-    color: "#7D70BA",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-  questionText: {
-    fontSize: 18,
-    paddingHorizontal: 45,
-    fontFamily: "Inter",
-    fontWeight: "400",
-    textAlign: "center",
-    justifyContent: "center",
-    zIndex: 1,
-    height: 80,
-  },
-  emoticonButtons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: 50,
+    marginBottom: 100,
     gap: 15,
   },
-  moodButton: {
+  skinProblemButtons: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
-    backgroundColor: "rgba(128,128,128,0.25)",
-    height: 40,
-    paddingHorizontal: 12,
-    paddingVertical: 5,
-    borderRadius: 15,
-    gap: 5,
-  },
-  skinHealthSection: {
-    flex: 1,
-    backgroundColor: "white",
-    paddingVertical: 14,
-    paddingHorizontal: 16,
-    marginTop: 20,
-    margin: 25,
-    borderRadius: 14,
+    zindex: 1,
     elevation: 4,
-    justifyContent: "center",
+    borderRadius: 14,
   },
-  skinHealthText: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 10,
-  },
-  analyzeButton: {
-    backgroundColor: "white",
-    elevation: 3,
-    marginHorizontal: 50,
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    borderRadius: 20,
-    flexDirection: "row",
-    textAlign: "center",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 5,
+  recommenderText: {
+    position: "absolute",
+    left: "35%",
   },
   toolbarIcons: {
     height: 22,
@@ -319,7 +200,7 @@ const styles = StyleSheet.create({
   },
   bottomToolbar: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "center",
     borderTopColor: "#ccc",
     paddingVertical: 24,
     position: "absolute",
@@ -331,6 +212,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     borderTopLeftRadius: 40,
     borderTopRightRadius: 40,
+    gap: 100,
   },
   cameraIconContainer: {
     backgroundColor: "#7D70BA",
